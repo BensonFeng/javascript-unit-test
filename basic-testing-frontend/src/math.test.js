6a9fb889c131b26b0112b2ab25cbde47dbcbeecb
BensonFeng <bensonfeng@outlook.com>
2022-06-11 18:29:35 -0400

    add more test case

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -14,3 +14,22 @@ it("should summarize all number values in an array", () => {
   );
   expect(result).toBe(expectedResult);
 });
+
+it("should yield NaN if a least one invalid number id provided", () => {
+  const inputs = ["invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numric string values is provided", () => {
+  const numbers = ["1", "2", "3"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduceRight(
+    (prevValue, curValue) => +prevValue + +curValue
+  );
+  expect(result).toBe(expectedResult);
+});
