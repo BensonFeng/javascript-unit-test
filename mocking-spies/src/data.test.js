17bd6db41d144c3e1fbdcbff680cc69c29fa5c64
BensonFeng <bensonfeng@outlook.com>
2022-06-15 19:26:02 -0400

    more mocking functionality

--- mocking-spies/src/data.test.js
+++ mocking-spies/src/data.test.js
@@ -3,7 +3,8 @@ import { generateReportData } from "./data";
 
 describe("generateReportData()", () => {
   it("should execute logFn if provided", () => {
-    const logger = vi.fn(() => 0);
+    const logger = vi.fn();
+    //logger.mockImplementationOnce(()=>{})
     generateReportData(logger);
     expect(logger).toBeCalled();
   });
