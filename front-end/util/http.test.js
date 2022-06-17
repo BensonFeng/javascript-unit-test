ae4f65a64a47bb757698e5c22d2f091e2c13e5ac
BensonFeng <bensonfeng@outlook.com>
2022-06-17 16:12:18 -0400

    test specific mocking login

--- front-end/util/http.test.js
+++ front-end/util/http.test.js
@@ -5,6 +5,9 @@ const testResponseData = { testKey: "testData" };
 
 const testFetch = vi.fn((url, options) => {
   return new Promise((resolve, reject) => {
+    if (typeof options.body !== "string") {
+      return reject("Not a string.");
+    }
     const testResponse = {
       okay: true,
       json() {
@@ -22,4 +25,16 @@ vi.stubGlobal("fetch", testFetch);
 it("should return any available response data", () => {
   const testData = { key: "test" };
   return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
+
+it("should convert the provided data to JSON before sending to request", async () => {
+  const testData = { key: "test" };
+
+  let errorMessage;
+  try {
+    await sendDataRequest(testData);
+  } catch (error) {
+    errorMessage = error;
+  }
+  expect(errorMessage).not.toBe("Not a string.");
 });
